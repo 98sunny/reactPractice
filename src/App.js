7d4cf03d2f2a431bed2415ab26f0a68c856b2db7
98sunny <sunnysingh.hj07@gmail.com>
2021-06-01 21:04:59 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react'
 function Car() {
   return <h2>Hi, I am also a Car!</h2>;
 }
+function Bike() {
+  return <h2>Hi, I am also a Bike!</h2>;
+}
 function Creator(){
   return <h2>This is made by Sunny</h2>;
-  
 }
 function App(){
   return(
     <div>
         <Creator/>
         <Car/>
+        <Bike/>
     </div>
    
     
