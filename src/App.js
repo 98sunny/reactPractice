57207f9f4342ac5a86a3763fc2146e460a9683d4
98sunny <sunnysingh.hj07@gmail.com>
2021-06-01 20:27:21 -0400

    changes made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+function Car() {
+  return <h2>Hi, I am also a Car!</h2>;
 }
-
-export default App;
+function Creator(){
+  return <h2>This is made by Sunny</h2>;
+  
+}
+function App(){
+  return(
+    <div>
+        <Creator/>
+        <Car/>
+    </div>
+   
+    
+  )
+}
+export default App
